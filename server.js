bece851914deced2c5540c251c041ebbbcc4eb64
Jamal <jamalraja083@gmail.com>
2025-11-01 11:13:03 +0000

    Update path to  data

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const fs = require("fs").promises;
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const dataPath = path.join(__dirname, "allData.json");
 
 async function readNotes() {
-  const data = await fs.readFile("./allData.json", "utf8");
+  const data = await fs.readFile(dataPath, "utf8");
   if (!data) return [];
   return JSON.parse(data);
 }
@@ -13,7 +15,7 @@ async function readNotes() {
 async function createNewNote(noteObj) {
   const allNotes = await readNotes();
   allNotes.unshift(noteObj);
-  await fs.writeFile("./allData.json", JSON.stringify(allNotes, null, 2));
+  await fs.writeFile(dataPath, JSON.stringify(allNotes, null, 2));
 }
 
 async function updateNote(updatedNote) {
@@ -24,13 +26,13 @@ async function updateNote(updatedNote) {
       ? { ...note, ...updatedNote }
       : note
   );
-  await fs.writeFile("./allData.json", JSON.stringify(updatedNotes, null, 2));
+  await fs.writeFile(dataPath, JSON.stringify(updatedNotes, null, 2));
 }
 
 async function deleteNote(noteID) {
   const allNotes = await readNotes();
   const updatedNotes = allNotes.filter((note) => note.id != noteID);
-  await fs.writeFile("./allData.json", JSON.stringify(updatedNotes, null, 2));
+  await fs.writeFile(dataPath, JSON.stringify(updatedNotes, null, 2));
 }
 
 // ============ MIDDLEWARE ============
